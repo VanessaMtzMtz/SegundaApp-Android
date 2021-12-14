3c54e79d96e35dce0c23e3e8bd3d1c64e876da0b
VanessaMtzMtz <78155041+VanessaMtzMtz@users.noreply.github.com>
2021-12-14 16:37:30 -0600

    Problemas con Adaptador ListView

--- Numeros/app/src/main/java/com/example/numeros/MainActivity.kt
+++ Numeros/app/src/main/java/com/example/numeros/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.numeros
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etNumber1: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etNumber1 = findViewById(R.id.etNumber1)
     }
+
+    fun contButton(view: View){
+        if(etNumber1.text.toString()!= ""){
+            val numNum = etNumber1.text.toString().toInt()
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("numNum", numNum)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this, "Se necesita un número", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
 }
--- Numeros/app/src/main/java/com/example/numeros/MainActivity2.kt
+++ Numeros/app/src/main/java/com/example/numeros/MainActivity2.kt
@@ -2,10 +2,73 @@ package com.example.numeros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var etNumber2: EditText
+    private lateinit var bttnIngresaLista: Button
+    private lateinit var bttnOrdenaMenor: Button
+    private lateinit var bttnOrdenaMayor: Button
+    var numNum: Int =0
+    var temp: Long =0
+    val listaNumeros = ArrayList<Long>()
+    val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNumeros)
+    val lv: ListView = findViewById<ListView>(R.id.lv)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        etNumber2 = findViewById(R.id.etNumber2)
+        bttnIngresaLista = findViewById(R.id.bttnIngresaLista)
+        bttnOrdenaMayor = findViewById(R.id.bttnOrdenaMayor)
+        bttnOrdenaMenor = findViewById(R.id.bttnOrdenaMenor)
+        numNum = getIntent().getIntExtra("numNum",-1)
+        lv.adapter = adaptador
+    }
+
+    fun ingresaNum(view: View){
+        if(etNumber2.text.toString()!= ""){
+            for(i in 1 until numNum){
+                listaNumeros.add(etNumber2.text.toString().toLong())
+            }
+        }else{
+            Toast.makeText(this, "Se necesita ingresar al menos 1 número", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun ordenaMayor(view: View){
+        if(listaNumeros.isNotEmpty()){
+            for(i in 0 until numNum-1){
+                for(j in i+1 until numNum) {
+                    if(listaNumeros[i]>listaNumeros[j]){
+                        temp = listaNumeros[j]
+                        listaNumeros[j] = listaNumeros[i]
+                        listaNumeros[j]= temp
+                    }
+                }
+            }
+            //Mostrar en list view
+        }else{
+            Toast.makeText(this, "Se necesita ingresar al menos 1 número", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun ordenaMenor(view: View){
+        if(listaNumeros.isNotEmpty()){
+            for(i in 0 until numNum-1){
+                for(j in i+1 until numNum) {
+                    if(listaNumeros[i]<listaNumeros[j]){
+                        temp = listaNumeros[j]
+                        listaNumeros[j] = listaNumeros[i]
+                        listaNumeros[j]= temp
+                    }
+                }
+            }
+            //Mostrar en list view
+
+        }else{
+            Toast.makeText(this, "Se necesita ingresar al menos 1 número", Toast.LENGTH_LONG).show()
+        }
     }
 }
